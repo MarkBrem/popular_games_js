4bc5097a93032dd9e6678ac3d0f105795f3d001e
MarkBrem <129090130+MarkBrem@users.noreply.github.com>
2024-05-03 16:52:53 +0000

    Deploying to gh-pages from @ MarkBrem/popular_games_js@843df6ae67c3f93e122384abaf0870fee02b03ff 🚀

---
+++ index.d085d0f8.js
@@ -0,0 +1,2 @@
+const n=function(n){return n.map(`\n        <li id=${game.id}>\n            <h1 style="\n            ">${game.name}</h1>\n        </li>\n        `)};document.querySelector(".js-game-list").append(...n);
+//# sourceMappingURL=index.d085d0f8.js.map
