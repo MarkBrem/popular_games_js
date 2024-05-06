36dbecf0a6e656e2dab747a6ead6b3c7fc257da5
Yehor <yehor12344321@gmail.com>
2024-05-06 20:37:35 +0300

    Зберігає зміни

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+const dropdownBtnRef = document.querySelector('.dropdown-btn')
+const dropdownContentRef = document.querySelector('.dropdown-content')
+
+
+
+    dropdownBtnRef.addEventListener('click', onClick);
+
+    function onClick (event){
+        dropdownContentRef.classList.add('is-open')
+
+        dropdownBtnRef.addEventListener('click', removeOnClick);
+
+        function removeOnClick(event){
+
+                dropdownContentRef.classList.toggle('is-close')
+        }
+    
+    };
+
+    
+
+
+    
+
