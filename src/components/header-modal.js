f2ddc28971e30331cfebc583103fd0f2b347522a
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-05-10 20:21:32 +0300

    .

--- src/components/header-modal.js
+++ src/components/header-modal.js
@@ -1,15 +1,15 @@
 const openModalBtnRef = document.querySelector('[data-action="open-modal"]');
 const closeModalBtnRef = document.querySelector['[data-action="close-modal"]'];
 const backdropModalRef = document.querySelector('.modal-backdrop');
-console.log(openModalBtnRef)
+// console.log(openModalBtnRef)
 openModalBtnRef.addEventListener('click', onOpenModal);
 function onOpenModal() {
-    console.log('hello')
+    // console.log('hello')
     backdropModalRef.classList.add('show-modal');
 };
 closeModalBtnRef.addEventListener('click', onCloseModal);
 function onCloseModal() {
-    backdropModalRef.classList.remove('show-modal')
+    backdropModalRef.classList.remove('show-modal');
 };
 
 backdropModalRef.addEventListener('click', onBackdropClose);
@@ -18,7 +18,6 @@ function onBackdropClose(event) {
         onCloseModal();
     };
 };
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // import './components/gamesList'
-import './components/rock-scissor-spaper'
+// import './components/rock-scissor-spaper'
 import './components/header-modal'
