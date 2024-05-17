9c66003fc55f53d6b34a8de1feffbb357ddc41dc
Iryna <irasokolova25@ukr.net>
2024-05-17 20:27:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import "./components/time-calculator.js"
 import './components/gamesList'
+import './components/header'
 import './components/gues-num'
 import './components/rock-scissor-spaper'
+import './components/ourTeam'
+// import "./components/time-calculator.js"
 import './components/two-digit-calculator'
 import './components/header'
