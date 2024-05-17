49a09e02e3c8f71aba1efb51467af82aa011add1
Yehor <yehor12344321@gmail.com>
2024-05-17 17:56:36 +0300

    зберігає зміни

--- src/components/dino.js
+++ src/components/dino.js
@@ -1,12 +1,34 @@
-const dino = document.querySelector('.dino')
-const cactus = document.querySelector('.cactus')
+const dinosaurRef = document.querySelector('.dinosaur');
+const cactusRef = document.querySelector('.cactus');
+const btnStartRef = document.querySelector('.button-start')
 
-document.addEventListener('keydown', jump)
+        btnStartRef.addEventListener('click', onClick);
 
-function jump (){
-        dino.classList.add('jump')
+function onClick(){
+        cactusRef.style.animationPlayState = 'running'
 
-        setTimeout(function(){
-                dino.classList.remove('jump')
-        }, 300)
+        document.addEventListener('keydown', jump);
+
+        function jump (){
+                if(dinosaurRef.classList != 'jump'){
+                        dinosaurRef.classList.add('jump');
+                }
+                setTimeout(function(){
+                        dinosaurRef.classList.toggle('jump');
+                }, 300)
+        }
+            
 }
+
+let isAlive = setInterval(function() {
+        let dinosaurTopPos = parseInt(window.getComputedStyle(dinosaurRef).getPropertyValue('top'));
+        let cactusLeftPos = parseInt(window.getComputedStyle(cactusRef).getPropertyValue('left'));
+
+        if(cactusLeftPos < 50 && cactusLeftPos > 0 && dinosaurTopPos >= 140){
+                alert('Ви програли');
+                window.location.reload();
+        }
+},10)
+
+
+
