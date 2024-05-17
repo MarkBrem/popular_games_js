80822f3a8dab8e0cec83dddf089ba68638fe432d
Yehor <yehor12344321@gmail.com>
2024-05-17 19:24:25 +0300

    зберігає зміни

--- src/components/changeTheme.js
+++
@@ -1,9 +0,0 @@
-const bodyRef = document.querySelector('body');
-const buttonThemeRef = document.querySelector('.button-theme');
-
-  buttonThemeRef.addEventListener('click', onClick);
-
-    function onClick (event){
-      bodyRef.classList.toggle('night-theme');
-      dropdownBtnRef.classList.toggle('night-theme-btn')
-    };
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,37 @@
+const bodyRef = document.querySelector('body');
 const dropdownBtnRef = document.querySelector('.dropdown-btn')
 const dropdownContentRef = document.querySelector('.dropdown-content')
-
-
+const buttonThemeRef = document.querySelector('.button-theme');
+const moonIconRef = document.querySelector('.moon-icon');
+const sunIconRef = document.querySelector('.sun-icon');
 
 
     dropdownBtnRef.addEventListener('click', onClick);
 
     function onClick (event){
-        dropdownContentRef.classList.toggle('is-open')
+        dropdownContentRef.classList.toggle('is-open');
     
+    };
+
+
+  buttonThemeRef.addEventListener('click', changeOnClick);
+
+    function changeOnClick (event){
+      bodyRef.classList.toggle('night-theme');
+      dropdownBtnRef.classList.toggle('night-theme-btn');
+
+      if(bodyRef.classList.contains('night-theme')){
+        moonIconRef.style.display = 'none';
+        sunIconRef.style.display = 'inline';
+        buttonThemeRef.style.border = '1px solid white'
+        sunIconRef.style.textAlign = 'center';
+
+      }else{
+        moonIconRef.style.display = 'inline';
+        sunIconRef.style.display = 'none';
+        buttonThemeRef.style.border = '1px solid black'
+        moonIconRef.style.textAlign = 'center';
+      }
     };
 
 
