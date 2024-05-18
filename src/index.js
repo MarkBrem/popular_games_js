87fdbbda0462ba6d175e065d10b7c6b4e9308e6a
Mark <mgusanica@gmail.com>
2024-05-18 12:28:14 +0300

    try

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ import './components/rock-scissor-spaper'
 import './components/two-digit-calculator'
 import './components/header'
 import './components/dinosaur'
-
 import './components/modal-submit'
