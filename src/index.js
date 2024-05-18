8efa2d25225aeab268915074ff3996c924340b5f
Mark <mgusanica@gmail.com>
2024-05-18 12:31:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './components/gamesList'
+import './components/header'
 import './components/rock-scissor-spaper'
 import './components/gues-num'
 import './components/rock-scissor-spaper'
 import './components/two-digit-calculator'
+<<<<<<< Updated upstream
 import './components/header'
 import './components/dinosaur'
+=======
+import './components/dinosaur'
+>>>>>>> Stashed changes
