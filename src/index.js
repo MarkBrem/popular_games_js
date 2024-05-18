7dcdaf65b38e5488fccffdf89b910a685fa5e4ee
Mark <mgusanica@gmail.com>
2024-05-18 12:32:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import './components/gues-num'
 import './components/rock-scissor-spaper'
 import './components/two-digit-calculator'
 import './components/header'
-import './components/dinosaur'
+
 import './components/dinosaur'
