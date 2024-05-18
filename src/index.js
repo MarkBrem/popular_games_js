5b8c8804f60349cf75914cce61f517b6cc3ae4fc
Mark <mgusanica@gmail.com>
2024-05-18 12:25:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './components/gamesList'
+import './components/rock-scissor-spaper'
 import './components/gues-num'
 import './components/rock-scissor-spaper'
 import './components/two-digit-calculator'
+import './components/header'
+import './components/dinosaur'
 import './components/modal-submit'
