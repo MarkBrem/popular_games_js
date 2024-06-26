f70874aeea109c5dd6b735df97e9c32aa2c3675b
Iryna <irasokolova25@ukr.net>
2024-06-26 21:38:08 +0300

    some

--- src/components/modal-submit.js
+++ src/components/modal-submit.js
@@ -1,19 +1,19 @@
-const openModalBtn = document.querySelector('[data-action="open-modal"]');
+const formRef = document.querySelector('[data-action="open-modal"]');
 const closeModalBtn = document.querySelector('[data-action="close-modal"]');
 const backdrop = document.querySelector('.backdrop');
 
  
 
 
-openModalBtn.addEventListener('submit', onOpenModal)
-function onOpenModal(event){
-event.preventDefault();
-backdrop.classList.add('show-modal')
-
-
-
+formRef.addEventListener('submit', onOpenModal)
+function onOpenModal(event) {
+    event.preventDefault()
+    if (event.currentTarget.elements.mail.value !== '') {
+        backdrop.classList.add('show')
+    }
+ alert('Введіть свою ел.адресу')
 }
 
 closeModalBtn.addEventListener('click', onCloseModal)
 function onCloseModal() {
-backdrop.classList.remove('show-modal')}
+backdrop.classList.remove('show')}
