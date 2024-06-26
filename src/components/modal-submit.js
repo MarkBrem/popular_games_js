5a8fc768cf363a1f60f4749486e0b2edc216a074
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-06-26 21:26:56 +0300

    .

--- src/components/modal-submit.js
+++ src/components/modal-submit.js
@@ -1,14 +1,14 @@
-const openModalBtn = document.querySelector('[data-action="open-modal"]');
+const formRef = document.querySelector('[data-action="open-modal"]');
 const closeModalBtn = document.querySelector('[data-action="close-modal"]');
 const backdrop = document.querySelector('.backdrop');
 
  
 
 
-openModalBtn.addEventListener('submit', onOpenModal)
+formRef.addEventListener('submit', onOpenModal)
 function onOpenModal(event){
 event.preventDefault();
-backdrop.classList.add('show-modal')
+backdrop.classList.add('is-hidden')
 
 
 
@@ -16,4 +16,4 @@ backdrop.classList.add('show-modal')
 
 closeModalBtn.addEventListener('click', onCloseModal)
 function onCloseModal() {
-backdrop.classList.remove('show-modal')}
+backdrop.classList.remove('is-hidden')}
